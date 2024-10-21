645d8aca0aebe8e383c389da2f87f70088bcad6a
IBH05 <106049652+IBH05@users.noreply.github.com>
2024-10-21 15:02:43 -0600

    Update Asiento.java

--- # Proyecto Unidad 2/Asiento.java
+++ # Proyecto Unidad 2/Asiento.java
@@ -2,7 +2,7 @@ public class Asiento {
     private final int numero;
     private final String tipo;
     private boolean disponible;
-    private Boleto boleto; // Referencia al boleto asignado al asiento
+    private Boleto boleto;
 
     public Asiento(int numero, String tipo) {
         this.numero = numero;
@@ -26,17 +26,14 @@ public class Asiento {
         this.disponible = false;
     }
 
-    // Getter para el boleto asignado
     public Boleto getBoleto() {
         return boleto;
     }
 
-    // Método para cambiar la disponibilidad del asiento
     public void setDisponible(boolean disponible) {
         this.disponible = disponible;
     }
 
-    // Método para asignar un boleto al asiento y marcarlo como no disponible
     public void asignarBoleto(Boleto boleto) {
         this.boleto = boleto;
         this.disponible = false;
