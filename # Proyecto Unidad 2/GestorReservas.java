3bc17831a1ab2117f780c49f91dd6e3327a90406
IBH05 <106049652+IBH05@users.noreply.github.com>
2024-10-21 15:04:40 -0600

    Update GestorReservas.java

--- # Proyecto Unidad 2/GestorReservas.java
+++ # Proyecto Unidad 2/GestorReservas.java
@@ -1,24 +1,20 @@
 import java.util.List;
 
 public class GestorReservas {
-    private final List<Reservacion> reservas; // Marcar como final
+    private final List<Reservacion> reservas; 
 
-    // Constructor
     public GestorReservas(List<Reservacion> reservas) {
         this.reservas = reservas;
     }
 
-    // Método para agregar una reservación
     public void agregarReservacion(Reservacion reservacion) {
         reservas.add(reservacion);
     }
 
-    // Método para cancelar una reservación
     public boolean cancelarReservacion(Reservacion reservacion) {
         return reservas.remove(reservacion);
     }
 
-    // Obtener todas las reservaciones
     public List<Reservacion> getReservas() {
         return reservas;
     }
